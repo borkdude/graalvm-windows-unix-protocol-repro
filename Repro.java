621f239f9b60c38800df6682f237a9f0aeaec2af
Michiel Borkent <michielborkent@gmail.com>
2023-03-09 20:48:37 +0100

    socker

--- Repro.java
+++ Repro.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class Repro {
     public static void main(String [] args) throws Exception {
-        var addr = java.net.UnixDomainSocketAddress.of("/tmp/sock1");
+        var addr = java.net.UnixDomainSocketAddress.of("sock1");
         var runnable = new Runnable(){
                 public void run() {
                     try {
